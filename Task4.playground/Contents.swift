280b4fc2add135a950d3192970d778a7ea646342
Motorkin Vladimir <vmmotorkin@edu.hse.ru>
2022-01-27 18:07:09 +0300

    Task 4 done.

---
+++ Task4.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+func getOnceWrittenChars(string: String) -> Set<Character> {
+    // Символы, которые вообще встретились.
+    var usedChars = Set<Character>()
+    // Символы, которые встретились минимум 2 раза
+    var twiceUsedChars = Set<Character>()
+    for char in string {
+        if usedChars.contains(char) {
+            twiceUsedChars.insert(char)
+        } else {
+            usedChars.insert(char)
+        }
+    }
+    // Вычитая из появившихся символов появившиеся дважды получаем уникальные.
+    return usedChars.subtracting(twiceUsedChars)
+}
+
+let uniqChars = getOnceWrittenChars(string: "i like Swift")
+print("Unique characters:")
+print(uniqChars)
+
+
+
+
