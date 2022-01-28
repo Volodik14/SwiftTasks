016862fe230d6f9e01fb5a6b9e84f23c13cb700f
Motorkin Vladimir <vmmotorkin@edu.hse.ru>
2022-01-28 14:28:29 +0300

    Task13 impruved.

--- Task13.playground/Contents.swift
+++ Task13.playground/Contents.swift
@@ -1,16 +1,17 @@
 import UIKit
 
+// Функция для reduce.
+func countDigitsInString(_ count : Int, _ char : Character) -> Int {
+    return count + char.wholeNumberValue!
+}
+
 func isNumberLucky(num: Int) -> Bool {
     let strNum = String(num)
     let firstHalf = strNum.prefix(strNum.count / 2)
     let secondHalf = strNum.suffix(strNum.count / 2)
     // Считаем сумму цифр в числах.
-    let sum1 = firstHalf.reduce(0) { (count, char) in
-        count + char.wholeNumberValue!
-    }
-    let sum2 = secondHalf.reduce(0) { (count, char) in
-        count + char.wholeNumberValue!
-    }
+    let sum1 = firstHalf.reduce(0, countDigitsInString)
+    let sum2 = secondHalf.reduce(0, countDigitsInString)
     return sum1 == sum2
 }
 
